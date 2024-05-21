2aa9fe0f047be505ee7a28631672d4bdb0831493
jobinpankaj <163808037+jobinpankaj@users.noreply.github.com>
2024-05-21 19:34:16 +0530

    Invoice files
    Invoice files

--- src/Supplier/components/Invoice/invoice.js
+++ src/Supplier/components/Invoice/invoice.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Sidebar from "../../../CommonComponents/Sidebar/sidebar";
 import Header from "../../../CommonComponents/Header/header";
+import { useTranslation } from "react-i18next";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Card from 'react-bootstrap/Card';
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from 'react-bootstrap/Container';
-import generateReportImage from "../../assets/images/generate-report-icon.svg";
 import apis from "../../../CommonComponents/apis";
 import { toast } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import RadialChart from "../../../CommonComponents/Charts/RadialChart_in";
+import RadialChartPro from "../../../CommonComponents/Charts/RadialChart_pro";
 // import ReportsModal from "./Partials/ReportsModal";
 
 const token = localStorage.getItem("supplier_accessToken");
 const config = {
   headers: {
     Authorization: `Bearer ${token}`,
-    permission: "reports-view",
+    permission: "",
   },
 };
 const url = "";
@@ -24,7 +27,7 @@ const url = "";
 const Invoice = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState(null);
-
+  const { t, i18n } = useTranslation();
   // call to fetch data
   const fetchData = () => {
     apis
@@ -61,59 +64,121 @@ const Invoice = () => {
       <div className="container-fluid page-wrap product-manage">
         <div className="row height-inherit">
           <Sidebar userType={"supplier"} />
-
           <div className="col main p-0">
-            <Header title="Reports" />
+        <Header title={t("supplier.invoice_title")} />
             <div className="container-fluid page-content-box px-3 px-sm-4">
               <div className="row">
                 <div className="col">
                   <div className="card">
                     <div className="card-body">
                       <div className="row">
-                        <div className="col-sm-8 mb-4 mb-sm-0">
-                          Products
-                          <hr />
-
-                          <Container>
+                        <div className="col-sm-12 md-12 mb-4 mb-sm-0">
                                <Row>
-                                 <Col xs={6} md={4}>
+                                 <Col xs={6} sm={6} md={3}>
                                  <Card className="row-card1">
                                  <Card.Body>
-                                   <Card.Title>Card Title</Card.Title>
                                    <Card.Text>
-                                  --
+          <Col xs={12} md={4} className="float-left"> <FontAwesomeIcon icon="fa-solid fa-users" /></Col>
+          <Col xs={12} md={6} className="float-right">
+<p>156<br /> {t("supplier.invoice_customers")}</p></Col>
                                    </Card.Text>
-                                   <Button variant="primary">Go somewhere</Button>
+
                                  </Card.Body>
                                </Card>
                                  </Col>
-                                 <Col xs={6} md={4}>
+                                <Col xs={6} sm={6} md={3}>
                                  <Card className="row-card2">
                                  <Card.Body>
-                                   <Card.Title>Card Title</Card.Title>
                                    <Card.Text>
-                                  --
+<Col xs={12} md={4} className="float-left"> <FontAwesomeIcon icon="fa-solid fa-box" /></Col>
+<Col xs={12} md={6} className="float-right"> {t("supplier.invoice_products")}</Col>
                                    </Card.Text>
-                                   <Button variant="primary">Go somewhere</Button>
                                  </Card.Body>
                                  </Card>
                                  </Col>
-                                 <Col xs={6} md={4}>
+                                  <Col xs={6} sm={6} md={3}>
                                  <Card className="row-card3">
                                  <Card.Body>
-                                   <Card.Title>Card Title</Card.Title>
                                    <Card.Text>
-                                --
+<Col xs={12} md={4} className="float-left"> <FontAwesomeIcon icon="fa-solid fa-hand-holding-dollar" /></Col>
+<Col xs={12} md={6} className="float-right"> {t("supplier.invoice_paid")} </Col>
                                    </Card.Text>
-                                   <Button variant="primary">Go somewhere</Button>
+
                                  </Card.Body>
                                  </Card>
                                  </Col>
+                                <Col xs={6} sm={6} md={3}>
+                                 <Card className="row-card4">
+                                 <Card.Body>
+                                   <Card.Text>
+<Col xs={12} md={4} className="float-left"> <FontAwesomeIcon icon="fa-solid fa-sack-dollar" /></Col>
+<Col xs={12} md={6} className="float-right"> {t("supplier.invoice_unpaid")} </Col>
+                                   </Card.Text>
 
+                                 </Card.Body>
+                                 </Card>
+                                 </Col>
                                </Row>
-                             </Container>
+                               <hr />
+                               <Row>
+                                <Col xs={6} sm={6} md={3}>
+                        <a href="/supplier/invoice_list">
+                        <Card className="row-card5">
+                                 <Card.Body>
+                                   <Card.Text>
+<Col xs={12} md={4} className="float-left"> <FontAwesomeIcon icon="fa-solid fa-paste" /></Col>
+                                   <Col xs={12} md={6} className="float-right"> {t("supplier.invoice_total_invoice")} </Col>
+                                   </Card.Text>
+                                 </Card.Body>
+                               </Card> </a>
+                                 </Col>
+                                 <Col xs={6} sm={6} md={3}>
+                                   <a href="/supplier/invoice_list">
+                                 <Card className="row-card6">
+                                 <Card.Body>
+                                   <Card.Text>
+<Col xs={12} md={4} className="float-left"> <FontAwesomeIcon icon="fa-solid fa-file-invoice-dollar" /></Col>
+                                   <Col xs={12} md={6} className="float-right"> {t("supplier.invoice_paid")} </Col>
+                                   </Card.Text>
 
+                                 </Card.Body>
+                                 </Card></a>
+                                 </Col>
+                                 <Col xs={6} sm={6} md={3}>
+                                   <a href="/supplier/invoice_list">
+                                 <Card className="row-card7">
+                                 <Card.Body>
+                                   <Card.Text>
+<Col xs={12} md={4} className="float-left"> <FontAwesomeIcon icon="fa-solid fa-file-circle-xmark" /></Col>
+                                   <Col xs={12} md={6} className="float-right">  {t("supplier.invoice_unpaid")} </Col>
+                                   </Card.Text>
 
+                                 </Card.Body>
+                                 </Card>
+                                 </a>
+                                 </Col>
+                                 <Col xs={6} sm={6} md={3}>
+                                   <a href="/supplier/invoice_list">
+                                 <Card className="row-card8">
+                                 <Card.Body>
+                                   <Card.Text>
+<Col xs={12} md={4} className="float-left"> <FontAwesomeIcon icon="fa-solid fa-file-circle-exclamation" /></Col>
+                                   <Col xs={12} md={6} className="float-right">{t("supplier.invoice_pending")}</Col>
+                                   </Card.Text>
+                                 </Card.Body>
+                                 </Card>
+                                 </a>
+                                 </Col>
+                               </Row>
+                               <hr />
+<Row>
+       <Col xs={6} md={6}>
+    <RadialChart />
+         </Col>
+         <Col xs={6} md={6}>
+      <RadialChartPro />
+           </Col>
+</Row>
                         </div>
                       </div>
                     </div>
